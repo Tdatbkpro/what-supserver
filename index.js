9f23d6f57b6eb59e7a1677dcd6d12eb9770f693f
Tdatbkpro <trandat22062004@gmail.com>
2025-07-16 10:13:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const crypto = require('crypto');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/get-signature', (req, res) => {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const paramsToSign = `timestamp=${timestamp}`;
+  const signature = crypto
+    .createHash('sha1')
+    .update(paramsToSign + process.env.CLOUDINARY_API_SECRET)
+    .digest('hex');
+
+  res.json({
+    signature,
+    timestamp,
+    apiKey: process.env.CLOUDINARY_API_KEY,
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  });
+});
+
+app.listen(3000, () => {
+  console.log('🚀 Server running on http://localhost:3000');
+});
